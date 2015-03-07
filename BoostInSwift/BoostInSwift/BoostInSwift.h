123d9312e26d1bb9e1b036e0dcb0b40e74f6e90a
David Steuber <dsteuber@gmail.com>
2015-03-06 19:16:57 -0500

    Initial project creation.

---
+++ BoostInSwift/BoostInSwift/BoostInSwift.h
@@ -0,0 +1,19 @@
+//
+//  BoostInSwift.h
+//  BoostInSwift
+//
+//  Created by David Steuber on 3/6/15.
+//  Copyright (c) 2015 David Steuber. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BoostInSwift.
+FOUNDATION_EXPORT double BoostInSwiftVersionNumber;
+
+//! Project version string for BoostInSwift.
+FOUNDATION_EXPORT const unsigned char BoostInSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BoostInSwift/PublicHeader.h>
+
+
